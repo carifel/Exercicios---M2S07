37a67edc64d8ed6c5345b89c1fe8e718a5f5c07b
Carina Felippus <carifel@gmail.com>
2024-08-12 18:34:24 -0300

    Ajuste final

--- src/routes/professores.routes.js
+++ src/routes/professores.routes.js
@@ -7,6 +7,6 @@ const professoresRoutes = new Router()
 professoresRoutes.post('/', ProfessorController.criar)
 professoresRoutes.get('/',  ProfessorController.listarPorParametro)
 professoresRoutes.put('/:id', ProfessorController.atualizar)
-
+professoresRoutes.delete('/:id', ProfessorController.deletar)
 
 module.exports = professoresRoutes
